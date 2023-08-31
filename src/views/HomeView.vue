947d26481f94c25070fbc8468c24bf33da297bad
koehdami <damiankoehler2612@gmailcom>
2023-08-31 15:36:38 +0200

    clear create input, hide password

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,10 +43,10 @@ taskStore.fetchTasks()
 </script>
 
 <template>
-  <div v-if="userStore.user">
+  <div class="mt-3" v-if="userStore.user">
     <div class="container mb-2">
       <div class="row">
-        <div class="col">
+        <div class="col p-0">
           <h2>You logged in as: {{ userStore.user.user.email }}</h2>
           <p>
             Do you want to
@@ -76,7 +76,7 @@ taskStore.fetchTasks()
             />
             <div class="input-group-append">
               <button
-                @click="taskStore.createTask(title, userStore.user.user.id)"
+                @click="taskStore.createTask(title, userStore.user.user.id); title=''"
                 class="btn btn-success ms-1"
                 type="button"
               >
@@ -115,7 +115,7 @@ taskStore.fetchTasks()
             <button @click="taskStore.editTask(task.id); editToggle(task.id, true)" class="btn btn-success w-100">Save</button>
           </div>
           <div :id="'deleteBtn_' + task.id" class="col-sm-2 col-3 px-1">
-            <button @click="taskStore.deleteTask(task.id)" class="btn btn-danger w-100">Delete</button>
+            <button @click="taskStore.deleteTask(task.id); editToggle(task.id, true);" class="btn btn-danger w-100">Delete</button>
           </div>
         </div>
       </div>
@@ -129,8 +129,8 @@ taskStore.fetchTasks()
     <div class="container">
       <div class="row d-flex justify-content-center">
         <div v-if="register" class="col-4 d-flex flex-column gap-2">
-          <input class="form-control" placeholder="Write your email" v-model="email" />
-          <input class="form-control" placeholder="Write your password" v-model="password" />
+          <input class="form-control" type="text" placeholder="Write your email" v-model="email" />
+          <input class="form-control" type="password" placeholder="Write your password" v-model="password" />
           <div>
             <button class="btn btn-primary w-100" @click="userStore.loginUser(email, password)">Login</button>
             <p style="font-size:14px;" class="fw-lighter text-end my-1">
@@ -149,9 +149,9 @@ taskStore.fetchTasks()
           v-else
           class="col-4 d-flex flex-column justify-content-center gap-2"
         >
-          <input class="form-control" placeholder="Write your email" v-model="email" />
-          <input class="form-control" placeholder="Write your password" v-model="password" />
-          <input class="form-control" placeholder="Confirm your password" v-model="passwordConfirm" />
+          <input class="form-control" type="text" placeholder="Write your email" v-model="email" />
+          <input class="form-control" type="password" placeholder="Write your password" v-model="password" />
+          <input class="form-control" type="password" placeholder="Confirm your password" v-model="passwordConfirm" />
           <div>
             <button
               class="btn btn-primary w-100"
