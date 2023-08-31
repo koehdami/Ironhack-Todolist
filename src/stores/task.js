0af0866abf4aef8533c70ad6fb66dba4589865e9
koehdami <damiankoehler2612@gmailcom>
2023-08-31 14:34:10 +0200

    added edit, save and remove feature

--- src/stores/task.js
+++ src/stores/task.js
@@ -5,20 +5,15 @@ import { ref } from "vue"
 export const useTaskStore = defineStore("taskStore", () => {
   const tasks = ref([]);
 
-  const fetchTasks = async (id) => {
+  const fetchTasks = async () => {
     const { data, error } = await supabase
       .from('tasks')
       .select()
+      .order('id', { ascending: false })
 
     if (error) console.log("Error: ", error);
     else{
-      let userTasks = []
-      for(let entrie of data){
-        if(entrie.user_id == id){
-          userTasks.push(entrie)
-        }
-      }
-      tasks.value = userTasks;
+      tasks.value = data;
     }
     console.log("tasks: ", tasks.value);
   }
@@ -28,17 +23,43 @@ export const useTaskStore = defineStore("taskStore", () => {
     .from("tasks")
     .insert({user_id:id, title:input})
     if(error) console.log(error)
-    else fetchTasks(id)
+    else fetchTasks()
+  }
+
+  const deleteTask = async (id) => {
+    const { error } = await supabase
+    .from('tasks')
+    .delete()
+    .eq('id', id)
+    if(error) console.log(error)
+    else fetchTasks()
+  }
+
+  // get value from currently active edit input and update in DB
+  const editTask = async (id) =>{
+    const updateValue = document.querySelector(`input[name="taskInput_${id}"]`)
+    const title = document.querySelector("#taskTitle_" + id)
+    if(updateValue){
+      const { data, error } = await supabase
+      .from('tasks')
+      .update({ "title": updateValue.value })
+      .eq('id', id)
+      .select()
+      if(error) console.log("Error: " + error)
+      else console.log(data)
+    }
+    title.innerHTML = updateValue.value
   }
   
 
-const updateTaskValue = async (updateValue, matchValue, id) => {
+const updateTaskValue = async (updateValue, matchValue) => {
   const { data, error } = await supabase
   .from('tasks')
   .update({"is_complete": updateValue })
   .eq("id", matchValue)
   .select()
   if(error) console.log("Error" + error)
+  else fetchTasks()
 }
-  return { tasks, fetchTasks, createTask, updateTaskValue}
+  return { tasks, fetchTasks, createTask, updateTaskValue, deleteTask, editTask}
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,45 @@
 <script setup>
 import { useUserStore } from "@/stores/user";
 import { useTaskStore } from "@/stores/task";
-import { ref } from 'vue';
+import { ref } from "vue";
 
-
-const register = ref(true)
+const register = ref(true);
 
 const userStore = useUserStore();
 const taskStore = useTaskStore();
 
-
 const email = ref("");
 const password = ref("");
-const passwordConfirm = ref("")
-
+const passwordConfirm = ref("");
 
 const swapMask = () => {
-  register.value = !register.value
-  nullValues()
-}
+  register.value = !register.value;
+  nullValues();
+};
 
 const nullValues = () => {
-  email.value = ""
-  password.value = ""
-  passwordConfirm.value = ""
-  userStore.message = ""
+  email.value = "";
+  password.value = "";
+  passwordConfirm.value = "";
+  userStore.message = "";
+};
+
+
+const editToggle = (id, edit) => {
+  const taskRow = document.querySelector(`taskRow_${id}`)
+  if(edit){
+    document.querySelector(`#taskRow_${id}`).classList = "row"
+    document.querySelector(`#taskRowEdit_${id}`).classList = "d-none"
+  }else{
+    document.querySelector(`#taskRow_${id}`).classList = "d-none"
+    document.querySelector(`#taskRowEdit_${id}`).classList = "row d-flex justify-content-center"
+  }
 }
 
+// editTaskTitle -> change p to input and get value from 
 
+
+taskStore.fetchTasks()
 </script>
 
 <template>
@@ -36,62 +48,132 @@ const nullValues = () => {
       <div class="row">
         <div class="col">
           <h2>You logged in as: {{ userStore.user.user.email }}</h2>
-          <p>Do you want to <button class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0" @click="userStore.logoutUser(); nullValues();">logout?</button></p>
+          <p>
+            Do you want to
+            <button
+              class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0"
+              @click="
+                userStore.logoutUser();
+                nullValues();
+              "
+            >
+              logout?
+            </button>
+          </p>
         </div>
       </div>
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-4">
+        <div class="col-sm-5 col-8 p-sm-0 px-2">
           <div class="input-group mb-3">
-            <input minlength="4" type="text" v-model="title" class="form-control" placeholder="Write new task">
+            <input
+              minlength="4"
+              type="text"
+              v-model="title"
+              class="form-control"
+              placeholder="Write new task"
+            />
             <div class="input-group-append">
-            <button @click="taskStore.createTask(title, userStore.user.user.id)" class="btn btn-success ms-1" type="button">Create</button>
+              <button
+                @click="taskStore.createTask(title, userStore.user.user.id)"
+                class="btn btn-success ms-1"
+                type="button"
+              >
+                Create
+              </button>
             </div>
           </div>
         </div>
-        <div class="col d-flex justify-content-end" style="max-height:38px;">
-          <button @click="taskStore.fetchTasks(userStore.user.user.id)" class="btn btn-primary me-2">Refresh</button>
-        </div>
-      </div>
-
-      </div>
-      <div class="container text-center border">
-          <div v-for="task in taskStore.tasks" class="row py-3" id="keys">
-              <div class="col" >
-                {{ task.title }}
-                {{ task.is_complete }}
-                {{ task.id }}
-              </div>
-              <div class="col" >
-                <input v-if="task.is_complete" @click="taskStore.updateTaskValue(false, task.id, userStore.user.user.id)" checked type="checkbox" name="checkbox">
-                <input v-else type="checkbox" @click="taskStore.updateTaskValue(true, task.id, userStore.user.user.id)" name="checkbox" :id="task.id">
-              </div>
-          </div>
       </div>
     </div>
-  <div v-else>
-    <p class="h2 mt-5 text-center">User Authentication</p> 
-    <p class="h3 text-center text-danger mb-4" v-if="userStore.message">{{ userStore.message }}</p> 
-    <div class="container">
-      <div class="row d-flex justify-content-center">
-        <div v-if="register" class="col-4 d-flex flex-column gap-2">
-          <input placeholder="Write your email" v-model="email">
-          <input placeholder="Write your password" v-model="password">
-          <button @click="userStore.loginUser(email, password)">Login</button>
-          <p class="text-end">Want to <button class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0" @click="swapMask">Register</button> insted?</p>
+    <div class="container text-center border p-0">
+      <div v-for="task in taskStore.tasks" class="container-fluid py-3" id="keys">
+        <div :id="'taskRow_' + task.id" class="row">
+          <div :id="'editBtn_' + task.id" class="col-2">
+            <button @click="editToggle(task.id, false)" class="btn btn-primary">Edit</button>
+          </div>
+          <div class="col">
+            <p class="d-flex justify-content-center align-items-center h-100 m-0" :id="'taskTitle_' + task.id">{{ task.title }}</p>
+          </div>
+          <div class="col d-flex justify-content-center align-items-center">
+            <input
+              @click="taskStore.updateTaskValue(task.is_complete, task.id)"
+              :checked="task.is_complete"
+              type="checkbox"
+              name="checkbox"
+              class="checkbox-round"
+              :id="'checkbox_' + task.id"
+            />
+          </div>
         </div>
-        <div v-else class="col-4 d-flex flex-column justify-content-center gap-2">
-          <input placeholder="Write your email" v-model="email">
-          <input placeholder="Write your password" v-model="password">
-          <input placeholder="Cofirm your password" v-model="passwordConfirm">
-          <button @click="userStore.createNewUser(email, password, passwordConfirm)">Create Account</button>
-          <p class="text-end">Want to <button class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0" @click="swapMask">login</button> insted?</p>
+        <div :id="'taskRowEdit_' + task.id" class="d-none">
+          <div class="col-sm-4 col-6">
+            <input class="form-control" type="text" :value=task.title :name="'taskInput_' + task.id">
+          </div>
+          <div :id="'saveBtn_' + task.id" class="col-sm-2 col-3 px-1">
+            <button @click="taskStore.editTask(task.id); editToggle(task.id, true)" class="btn btn-success w-100">Save</button>
+          </div>
+          <div :id="'deleteBtn_' + task.id" class="col-sm-2 col-3 px-1">
+            <button @click="taskStore.deleteTask(task.id)" class="btn btn-danger w-100">Delete</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
+  <div v-else>
+    <p class="h1 mt-5 text-center">User Authentication</p>
+    <p class="h3 text-center text-danger mb-4" v-if="userStore.message">
+      {{ userStore.message }}
+    </p>
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div v-if="register" class="col-4 d-flex flex-column gap-2">
+          <input class="form-control" placeholder="Write your email" v-model="email" />
+          <input class="form-control" placeholder="Write your password" v-model="password" />
+          <div>
+            <button class="btn btn-primary w-100" @click="userStore.loginUser(email, password)">Login</button>
+            <p style="font-size:14px;" class="fw-lighter text-end my-1">
+              Want to
+              <button
+                class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0"
+                @click="swapMask"
+              >
+                Register
+              </button>
+              insted?
+            </p>
+          </div>
+        </div>
+        <div
+          v-else
+          class="col-4 d-flex flex-column justify-content-center gap-2"
+        >
+          <input class="form-control" placeholder="Write your email" v-model="email" />
+          <input class="form-control" placeholder="Write your password" v-model="password" />
+          <input class="form-control" placeholder="Confirm your password" v-model="passwordConfirm" />
+          <div>
+            <button
+              class="btn btn-primary w-100"
+              @click="userStore.createNewUser(email, password, passwordConfirm)"
+            >
+              Create Account
+            </button>
+            <p style="font-size:14px;" class="fw-lighter text-end my-1">
+              Want to
+              <button
+                class="bg-transparent border-0 text-primary text-decoration-underline w-auto m-0 p-0"
+                @click="swapMask"
+              >
+                login
+              </button>
+              insted?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- <ul>
     <li v-for="task in taskStore.tasks">
@@ -101,7 +183,24 @@ const nullValues = () => {
 </template>
 
 <style>
-  #keys:nth-child(even){
-    background: rgba(228, 228, 228, 0.26);
-  }
+#keys:nth-child(even) {
+  background: rgba(228, 228, 228, 0.26);
+}
+
+.checkbox-round {
+    width: 25px;
+    height: 25px;
+    background-color: white;
+    border-radius: 50%;
+    vertical-align: middle;
+    border: 2px solid #ddd;
+    appearance: none;
+    -webkit-appearance: none;
+    outline: none;
+    cursor: pointer;
+}
+
+.checkbox-round:checked {
+    background-color: #66bb6a;
+}
 </style>
