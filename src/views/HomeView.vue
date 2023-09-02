a71c8770f15faf6791a5ef3ab51e233659cecf78
koehdami <damiankoehler2612@gmailcom>
2023-09-02 07:00:04 +0200

    mobile responsiveness fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,7 +44,7 @@ const editToggle = (id, edit) => {
   <div class="mt-3" v-if="userStore.user">
     <div class="container mb-2">
       <div class="row">
-        <div class="col p-0">
+        <div class="col p-sm-0">
           <h2>You logged in as: {{ userStore.user.user.email }}</h2>
           <p>
             Do you want to
@@ -130,7 +130,7 @@ const editToggle = (id, edit) => {
     </p>
     <div class="container">
       <div class="row d-flex justify-content-center">
-        <div v-if="register" class="col-4 d-flex flex-column gap-2">
+        <div v-if="register" class="col-8 col-sm-4 d-flex flex-column gap-2">
           <input class="form-control" type="text" placeholder="Write your email" v-model="email" />
           <input class="form-control" type="password" placeholder="Write your password" v-model="password" />
           <div>
@@ -149,7 +149,7 @@ const editToggle = (id, edit) => {
         </div>
         <div
           v-else
-          class="col-4 d-flex flex-column justify-content-center gap-2"
+          class="col-8 d-flex flex-column justify-content-center gap-2"
         >
           <input class="form-control" type="text" placeholder="Write your email" v-model="email" />
           <input class="form-control" type="password" placeholder="Write your password" v-model="password" />
